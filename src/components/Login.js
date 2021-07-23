c7f1d8ffb0c4d436d20acb3942e1881acd898a8f
Cara Locke <cara.m.locke@gmail.com>
2021-07-23 07:40:09 -0400

    Completed building Login.js.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,65 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const Login = () => {
+const Login = (props) => {
+  const { push } = props.history
+  const initialFormValues = {
+    username: '',
+    password: ''
+  }
+  const [ formValues, setFormValues ] = useState(initialFormValues)
+  
+  const [ error, setError ] = useState('');
+  //replace with error state
+  
+  const handleChange = e => {
+    setFormValues({
+      [e.target.name] : e.target.value
+    })
+  }
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-
-  const error = "";
-  //replace with error state
-
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/login', formValues)
+         .then(res => {
+           console.log('login res: ', res)
+           setError('')
+           localStorage.setItem('token', res.data.payload)
+           push('/bubbles')
+         })
+         .catch(err => {
+           setError('Username or Password not valid')
+         })
+  }
+  
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <h2>Log in: </h2>
+        <form onSubmit={handleSubmit}>
+          <label>Username: 
+            <input 
+             type='text'
+             name='username'
+             id='username'
+             value={formValues.username}
+             onChange={handleChange}
+            />
+          </label>
+          <label>Password: 
+            <input 
+             type='password'
+             name='password'
+             id='password'
+             value={formValues.password}
+             onChange={handleChange}
+            />
+          </label>
+          <button id='submit'>Log in</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
