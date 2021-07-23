db91d41ed098de216e35459ae6add15305147eb5
Cara Locke <cara.m.locke@gmail.com>
2021-07-23 10:55:26 -0400

    Completed half of BubblePage.test.js. Will come back to second half.

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -3,11 +3,36 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
+import fetchColorService from '../services/fetchColorService'
+jest.mock('../services/fetchColorService')
+
+const testColors = [
+    {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 1,
+    },
+    {
+      color: "limegreen",
+      code: {
+        hex: "#99ddbc",
+      },
+      id: 2,
+    }
+]
 
 test("Renders without errors", ()=> {
-    
+    fetchColorService.mockResolvedValue(testColors)
+    render(<BubblePage/>)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    // const colors = await screen.findAllByTestId()
+    fetchColorService.mockResolvedValue(testColors);
+    render(<BubblePage />);
+    const colors =  screen.findAllByTestId(/color/i)
+    expect(colors).toHaveLength(2)
 });
